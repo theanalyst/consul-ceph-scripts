48e02fdffc5f8e3fbee9b33f4a324635a4a45d0d
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-02-21 15:02:22 +0530

    first cut at osd_mon reading the socket

---
+++ osd_mon.py
@@ -0,0 +1,35 @@
+import socket
+import struct
+import json
+import glob
+import logging
+
+logging.basicConfig(filename='/var/log/ceph/stats-osd-mon.log',
+                    level=logging.INFO)
+
+
+def query_osd_sock(osd_sock, query):
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.STREAM)
+        sock.connect(osd_sock)
+    except socket.error, e:
+        logging.info("Connection with socket failed with %s" % e)
+        return None
+
+    sock.sendall(query)
+
+    try:
+        length = struct.unpack('>i', sock.recv(4))[0]
+        json_data = json.loads(sock.recv(length))
+    except Exception as e:
+        logging.info("JSON parsing failed with %s" % e)
+        return None
+    return json_data
+
+
+def main():
+    socks = glob.glob('/var/run/ceph/*osd*asok')
+    for sock in socks:
+        r = query_osd_sock(sock, '{\"prefix\": \"perf dump\"}\0')
+        if r is not None:
+            print r
